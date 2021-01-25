ea4eb4e439e94825451c40789dd51229b57f4ce1
krevedkin <frakestolstm@gmail.com>
2021-01-25 13:38:38 +0300

    removed pydantic config added old config

---
+++ config.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# script version 1.0
+import os
+from dotenv import load_dotenv
+
+# telegram part
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, "credentials/.env"))
+
+# this is list of users which have permissions for using this app
+WHITE_LIST = [
+    os.getenv("USER_1"),
+    os.getenv("USER_2"),
+    os.getenv("USER_3"),
+    os.getenv("USER_4"),
+    os.getenv("USER_5")
+]
+
+TOKEN = os.getenv("TOKEN")
+URL_21 = os.getenv("URL_21")
+URL_20 = os.getenv("URL_21")
+URL_TO = os.getenv("URL_to")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+
+if __name__ == '__main__':
+    print(URL_TO)
