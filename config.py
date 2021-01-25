8ede52d39e235cc886d9b41ed4d83660a73ed3ba
krevedkin <frakestolstm@gmail.com>
2021-01-25 14:59:38 +0300

    finally it works

--- config.py
+++ config.py
@@ -1,30 +1,26 @@
-# -*- coding: utf-8 -*-
-
-# script version 1.0
+from pydantic import BaseSettings
 import os
-from dotenv import load_dotenv
 
-# telegram part
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, "credentials/.env"))
+path_to_env = os.path.join(basedir, "credentials/.env")
 path_to_creds = os.path.join(basedir, "credentials/credentials.json")
 
 
-# this is list of users which have permissions for using this app
-WHITE_LIST = [
-    os.getenv("USER_1"),
-    os.getenv("USER_2"),
-    os.getenv("USER_3"),
-    os.getenv("USER_4"),
-    os.getenv("USER_5")
-]
+class Settings(BaseSettings):
+    TOKEN: str
+    URL_21: str
+    URL_20: str
+    URL_to: str
+    GUARANTEE_URL: str
+    WEBHOOK_URL: str
+    USER_1: str
+    USER_2: str
+    USER_3: str
+    USER_4: str
+    USER_5: str
 
-TOKEN = os.getenv("TOKEN")
-URL_21 = os.getenv("URL_21")
-URL_20 = os.getenv("URL_20")
-URL_TO = os.getenv("URL_to")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+    class Config:
+        env_file = path_to_env
 
-if __name__ == '__main__':
 
-    print(path_to_creds)
+settings = Settings()
